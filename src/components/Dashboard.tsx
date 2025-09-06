be51c8ffc9ff2ef2240421c4f25ffb0ac41b02a5
Fromundus <ninjadentist24@gmail.com>
2025-09-06 22:56:54 +0800

    notifications

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/AppSidebar";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { 
+  AlertTriangle,
+  Bell,
   Settings, 
   User
 } from "lucide-react";
@@ -11,6 +13,8 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel,
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { useAuth } from "@/store/auth";
+import api from "@/api/axios";
+import { format } from "date-fns";
 
 const pageNames: Record<string, string> = {
   "/": "Dashboard Overview",
@@ -55,6 +59,22 @@ export default function Dashboard() {
   
   const userName = user?.name?.split("").slice(0, 2).join("").toUpperCase();
 
+  const [notifications, setNotifications] = React.useState([]);
+
+  const fetchNotif = async () => {
+    try {
+      const res = await api.get('/notifications');
+      console.log(res);
+      setNotifications(res.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchNotif();
+  }, []);
+
   const handleLogout = async () => {
     try {
       const res = await logout();
@@ -64,6 +84,30 @@ export default function Dashboard() {
       console.log(err);
     }
   }
+
+  const renderNotifs = notifications?.map((item) => {
+    const data = JSON.parse(item.data);
+
+    return (
+      <div key={item.id}>
+        {item.title === "Summary" && <div className="bg-accent rounded p-2 text-sm" >
+          <div className="flex flex-col gap-2 p-2">
+            <span className="font-semibold">{item.title}</span>
+            <p><span className="text-red-500">{data.severe} Severe</span>, <span className="text-orange-500">{data.moderate} Moderate</span>, <span className="text-yellow-500">{data.at_risk} At Risk</span>, <span className="text-green-500">{data.healthy} Healthy</span> (out of {data.total} total).</p>
+            <span>{format(new Date(item.created_at), 'p')}</span>
+          </div>
+        </div>}
+
+        {item.title === "Warning" && <div className="bg-destructive rounded p-2 text-sm" >
+          <div className="flex flex-col gap-2 p-2">
+            <span className="font-semibold">{item.title}</span>
+            <p><span className="flex items-center gap-2"><AlertTriangle /> Alert:</span><span>{data.severe}</span> patients are currently in Severe condition. Immediate attention required.</p>
+            <span>{format(new Date(item.created_at), 'p')}</span>
+          </div>
+        </div>}
+      </div>
+    )
+  });
 
   return (
     <SidebarProvider>
@@ -81,6 +125,17 @@ export default function Dashboard() {
               </div>
             </div>
             <div className="flex items-center gap-4">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <div className="p-2 rounded hover:bg-accent relative">
+                    <Bell className="size-[22px]" />
+                    {notifications?.length > 0 && <div className="size-2 bg-red-500 absolute rounded-full top-1 right-1"></div>}
+                  </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-full flex-col space-y-2">
+                  {notifications?.length > 0 ? renderNotifs : <div className="p-6 text-center">No notifications.</div>}
+                </DropdownMenuContent>
+              </DropdownMenu>
               <ThemeToggle />
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
